3883ed7afc90b79db615c0c14639f08032070ad3
Evie Hoang <evie.h0325@gmail.com>
2023-12-04 19:06:12 -0800

    Updated index.html with routing.

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,4 @@
-import 'mdb-react-ui-kit/dist/css/mdb.min.css';
-import "@fortawesome/fontawesome-free/css/all.min.css";
+
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
