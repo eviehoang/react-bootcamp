b9dcc17d28a956978d7ea1defaf3e9c7cc978326
Evie Hoang <evie.h0325@gmail.com>
2023-12-04 21:05:51 -0800

    Updated custom CSS

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,16 +2,14 @@ import React from 'react';
 
 export default function Header() {
     return (
-        <div>
-            <div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 0.6)' }}>
-                <div className='d-flex m-5 p-3 justify-content-left align-items-center h-75'>
-                    <div className='text-white'>
-                        <h1 className='m-1'>Hi! I'm <u>Nhi Hoang</u> </h1>
-                        <h4 className='m-1'>It's nice to meet you!</h4>
-                        <h5 className='m-1'>Take a <b class="animate-flicker">click </b>around</h5>
-                    </div>
+        <div className="box">
+
+                <div className='text-white'>
+                    <h1 className='m-1'>Hi! I'm <u>Nhi Hoang</u> </h1>
+                    <h4 className='m-1'>It's nice to meet you!</h4>
+                    <h5 className='m-1'>Take a <b class="animate-flicker">click </b>around</h5>
                 </div>
-            </div>
+
         </div>
     );
 }
--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -30,7 +30,7 @@ export default function Nav() {
 
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            <a class="nav-link" aria-current="page" href="#">Home</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">About</a>
