e276bdb26b040110ae51a8d61d90e73cab0d7c52
Evie Hoang <evie.h0325@gmail.com>
2023-12-04 23:38:42 -0800

    Fixed card component display

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -16,7 +16,21 @@ export default function Cards() {
           </a>
         </div>
         <div className="card-body">
-          <h5 className="card-title">Card title</h5>
+          <h5 className="card-title">Project 1</h5>
+          <p className="card-text">Built with React.</p>
+          <a href="#!" className="btn btn-primary" data-mdb-ripple-init>Button</a>
+        </div>
+      </div>
+
+      <div className="card m-3">
+        <div className="bg-image hover-overlay" data-mdb-ripple-init data-mdb-ripple-color="light">
+          <img src="https://mdbcdn.b-cdn.net/img/new/standard/nature/114.webp" className="img-fluid" alt="Nature" />
+          <a href="#!">
+            <div className="mask" style={{ backgroundColor: 'rgba(251, 251, 251, 0.15)' }}></div>
+          </a>
+        </div>
+        <div className="card-body">
+          <h5 className="card-title">Project 2</h5>
           <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
           <a href="#!" className="btn btn-primary" data-mdb-ripple-init>Button</a>
         </div>
@@ -24,27 +38,13 @@ export default function Cards() {
 
       <div className="card m-3">
         <div className="bg-image hover-overlay" data-mdb-ripple-init data-mdb-ripple-color="light">
-          <img src="https://mdbcdn.b-cdn.net/img/new/standard/nature/111.webp" className="img-fluid" alt="Nature" />
+          <img src="https://mdbcdn.b-cdn.net/img/new/standard/nature/102.webp" className="img-fluid" alt="Nature" />
           <a href="#!">
             <div className="mask" style={{ backgroundColor: 'rgba(251, 251, 251, 0.15)' }}></div>
           </a>
         </div>
         <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#!" className="btn btn-primary" data-mdb-ripple-init>Button</a>
-        </div>
-      </div>
-
-      <div className="card m-3">
-        <div className="bg-image hover-overlay" data-mdb-ripple-init data-mdb-ripple-color="light">
-          <img src="https://mdbcdn.b-cdn.net/img/new/standard/nature/111.webp" className="img-fluid" alt="Nature" />
-          <a href="#!">
-            <div className="mask" style={{ backgroundColor: 'rgba(251, 251, 251, 0.15)' }}></div>
-          </a>
-        </div>
-        <div className="card-body">
-          <h5 className="card-title">Card title</h5>
+          <h5 className="card-title">Project 3</h5>
           <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
           <a href="#!" className="btn btn-primary" data-mdb-ripple-init>Button</a>
         </div>
@@ -53,13 +53,13 @@ export default function Cards() {
       {/* Second Row */}
       <div className="card m-3">
         <div className="bg-image hover-overlay" data-mdb-ripple-init data-mdb-ripple-color="light">
-          <img src="https://mdbcdn.b-cdn.net/img/new/standard/nature/111.webp" className="img-fluid" alt="Nature" />
+          <img src="https://mdbcdn.b-cdn.net/img/new/standard/nature/117.webp" className="img-fluid" alt="Nature" />
           <a href="#!">
             <div className="mask" style={{ backgroundColor: 'rgba(251, 251, 251, 0.15)' }}></div>
           </a>
         </div>
         <div className="card-body">
-          <h5 className="card-title">Card title</h5>
+          <h5 className="card-title">Project 4</h5>
           <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
           <a href="#!" className="btn btn-primary" data-mdb-ripple-init>Button</a>
         </div>
@@ -67,13 +67,13 @@ export default function Cards() {
 
       <div className="card m-3">
         <div className="bg-image hover-overlay" data-mdb-ripple-init data-mdb-ripple-color="light">
-          <img src="https://mdbcdn.b-cdn.net/img/new/standard/nature/111.webp" className="img-fluid" alt="Nature" />
+          <img src="https://mdbcdn.b-cdn.net/img/new/standard/nature/113.webp" className="img-fluid" alt="Nature" />
           <a href="#!">
             <div className="mask" style={{ backgroundColor: 'rgba(251, 251, 251, 0.15)' }}></div>
           </a>
         </div>
         <div className="card-body">
-          <h5 className="card-title">Card title</h5>
+          <h5 className="card-title">Project 5</h5>
           <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
           <a href="#!" className="btn btn-primary" data-mdb-ripple-init>Button</a>
         </div>
@@ -81,13 +81,13 @@ export default function Cards() {
 
       <div className="card m-3">
         <div className="bg-image hover-overlay" data-mdb-ripple-init data-mdb-ripple-color="light">
-          <img src="https://mdbcdn.b-cdn.net/img/new/standard/nature/111.webp" className="img-fluid" alt="Nature" />
+          <img src="https://mdbcdn.b-cdn.net/img/new/standard/nature/101.webp" className="img-fluid" alt="Nature" />
           <a href="#!">
             <div className="mask" style={{ backgroundColor: 'rgba(251, 251, 251, 0.15)' }}></div>
           </a>
         </div>
         <div className="card-body">
-          <h5 className="card-title">Card title</h5>
+          <h5 className="card-title">Project 6</h5>
           <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
           <a href="#!" className="btn btn-primary" data-mdb-ripple-init>Button</a>
         </div>
