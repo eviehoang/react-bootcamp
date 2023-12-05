08aef6ea8f1a78e2f06cbfb547a8dec25c77aad4
Evie Hoang <evie.h0325@gmail.com>
2023-12-05 00:11:31 -0800

    update vite

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/react-porfolio/', // Adjust 'your-app-name' to the actual name of your app
   plugins: [react()],
   server: {
     port: 3000,
