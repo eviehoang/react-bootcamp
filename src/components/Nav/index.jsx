3443ba5f42c6274a88fdf1cbbe8945c3e0d4b0d0
Evie Hoang <evie.h0325@gmail.com>
2023-12-04 19:15:48 -0800

    Updated to debug display.

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,46 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
-    MDBNavbar,
-    MDBNavbarNav,
-    MDBNavbarItem,
-    MDBNavbarLink,
-    MDBNavbarToggler,
-    MDBContainer,
-    MDBIcon,
-    MDBCollapse
+  MDBNavbar,
+  MDBNavbarNav,
+  MDBNavbarItem,
+  MDBNavbarLink,
+  MDBNavbarToggler,
+  MDBContainer,
+  MDBIcon,
+  MDBBtn,
+  MDBCollapse
 } from 'mdb-react-ui-kit';
 
 export default function Nav() {
-    return (
-<MDBNavbar expand='lg' light bgColor='white' sticky>
-<MDBContainer fluid>
-  <MDBNavbarToggler
-    aria-controls='navbarExample01'
-    aria-expanded='false'
-    aria-label='Toggle navigation'
-    onClick={() => setShowBasic(!showBasic)}
-  >
-    <MDBIcon fas icon='bars' />
-  </MDBNavbarToggler>
-  <MDBCollapse show={showBasic}>
-    <MDBNavbarNav right className='mb-2 mb-lg-0'>
-      <MDBNavbarItem active>
-        <MDBNavbarLink aria-current='page' href='#'>
-          Home
-        </MDBNavbarLink>
-      </MDBNavbarItem>
-      <MDBNavbarItem>
-        <MDBNavbarLink href='#'>Features</MDBNavbarLink>
-      </MDBNavbarItem>
-      <MDBNavbarItem>
-        <MDBNavbarLink href='#'>Pricing</MDBNavbarLink>
-      </MDBNavbarItem>
-      <MDBNavbarItem>
-        <MDBNavbarLink href='#'>About</MDBNavbarLink>
-      </MDBNavbarItem>
-    </MDBNavbarNav>
-  </MDBCollapse>
-</MDBContainer>
-</MDBNavbar>
-);
+  const [showBasic, setShowBasic] = useState(false);
+
+  return (
+    <header>
+      <MDBNavbar expand='lg' light bgColor='white' fixed>
+        <MDBContainer fluid>
+          <MDBNavbarToggler
+            aria-controls='navbarExample01'
+            aria-expanded='false'
+            aria-label='Toggle navigation'
+          >
+            <MDBIcon fas icon='bars' />
+          </MDBNavbarToggler>
+          <MDBCollapse show={showBasic}>
+            <MDBNavbarNav right className='mb-2 mb-lg-0'>
+              <MDBNavbarItem active>
+                <MDBNavbarLink aria-current='page' href='#'>
+                  Home
+                </MDBNavbarLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink href='#'>Features</MDBNavbarLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink href='#'>Pricing</MDBNavbarLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink href='#'>About</MDBNavbarLink>
+              </MDBNavbarItem>
+            </MDBNavbarNav>
+          </MDBCollapse>
+        </MDBContainer>
+      </MDBNavbar>
+    </header>
+  );
 }
