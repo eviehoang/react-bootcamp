fce626520bbb028e861dd4e07f641113147f1777
Evie Hoang <evie.h0325@gmail.com>
2024-01-04 12:50:29 -0800

    Add cursor style and remove unused code

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Nav from "./components/navbar";
 import Footer from "./components/footer";
 import { Outlet } from "react-router-dom";
 
+
 function App() {
   return (
     <main className="min-h-screen flex flex-col justify-between">
