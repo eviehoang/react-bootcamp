18535c819fa2ec5eb78ed243890845cd16587435
Evie Hoang <evie.h0325@gmail.com>
2024-01-04 20:51:34 -0800

    Update contact, projects, and resume pages

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,12 @@
 
 function Contact() {
     return (
-      <section>
-        <div className="flex items-end justify-center m-36 space-x-12">
-          <img src="https://64.media.tumblr.com/65dbd49e0eea427b0aef1a22d1f8f2d5/e9c959932c2f7833-a0/s400x600/b1e096432513ac1af910fa3ae70f4774e1ff6972.gif" alt="Working on it"></img>
-        </div>
-      </section>
+      <section className="flex items-center justify-center m-32 z-40">
+      <div className="text-center">
+      <h1 className="text-3xl font-bold text-center text-teal dark:text-white">Working on it!</h1>
+      <img src="https://media.tenor.com/-Yz3YJhJYcUAAAAi/mongmong-cute.gif" alt="Working on it"></img>
+      </div>
+    </section>
     );
   }
   
