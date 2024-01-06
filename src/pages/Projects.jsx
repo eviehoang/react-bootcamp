dfde77c2af8dbacfd998c58367e55ff3766e4065
Evie Hoang <evie.h0325@gmail.com>
2024-01-06 11:10:37 -0800

    Refactor font-family and add project cards. Updated Project page with code and cards.

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,10 +1,68 @@
+import recipeRolo from "../assets/project-images/recipe-rolodex.png"
+// import workRemote from "../assets/project-images/work-remote.png"
+import noteTaker from "../assets/project-images/note-taker.png"
 
 function Projects() {
   return (
-    <section className="flex items-center justify-center m-32 z-40">
-      <div className="text-center">
-      <h1 className="text-3xl font-bold text-center text-teal dark:text-white">Working on it!</h1>
-      <img src="https://media.tenor.com/-Yz3YJhJYcUAAAAi/mongmong-cute.gif" alt="Working on it"></img>
+    <section className="flex items-center flex-wrap justify-center m-8 px-8 z-40 gap-4">
+      <div className="m-auto container mb-0">
+        <h1 id="projectHeading">Projects</h1>
+      </div>
+
+      <div className="flex flex-wrap justify-between mt-0 gap-5 glassBox p-8">
+        <a href="https://recipe-rolodex-d7c0cb19d5d1.herokuapp.com/" className="projectCard">
+          <figure>
+            <img src={recipeRolo} alt="Recipe Rolodex Site" />
+            <figcaption>Recipe Rolodex
+            <p>Recipe Rolodex, created with REACT, and Bootstrap Material Design.</p>
+            </figcaption>
+          </figure>
+        </a>
+
+        <a href="https://work-remote-jsn-4d3b6593bab7.herokuapp.com/" className="projectCard">
+          <figure>
+            <img src={recipeRolo} alt="Work Remote Site" />
+            <figcaption>Work Remote
+            <p>An application created with Node.js, MySQL, Sequelize, Handlebars, and Bootstrap Material Design.</p>
+            </figcaption>  
+          </figure>
+        </a>
+
+        <a href="https://work-remote-jsn-4d3b6593bab7.herokuapp.com/" className="projectCard">
+          <figure>
+            <img src={noteTaker} alt="Work Remote Site" />
+            <figcaption>Work Remote
+            <p>An application created with Node.js, MySQL, Sequelize, Handlebars, and Bootstrap Material Design.</p>
+            </figcaption>  
+          </figure>
+        </a>
+
+        <a href="https://recipe-rolodex-d7c0cb19d5d1.herokuapp.com/" className="projectCard">
+          <figure>
+            <img src={recipeRolo} alt="Recipe Rolodex Site" />
+            <figcaption>Recipe Rolodex
+            <p>Recipe Rolodex, created with REACT, and Bootstrap Material Design.</p>
+            </figcaption>
+          </figure>
+        </a>
+
+        <a href="https://work-remote-jsn-4d3b6593bab7.herokuapp.com/" className="projectCard">
+          <figure>
+            <img src={recipeRolo} alt="Work Remote Site" />
+            <figcaption>Work Remote
+            <p>An application created with Node.js, MySQL, Sequelize, Handlebars, and Bootstrap Material Design.</p>
+            </figcaption>  
+          </figure>
+        </a>
+
+        <a href="https://work-remote-jsn-4d3b6593bab7.herokuapp.com/" className="projectCard">
+          <figure>
+            <img src={noteTaker} alt="Work Remote Site" />
+            <figcaption>Work Remote
+            <p>An application created with Node.js, MySQL, Sequelize, Handlebars, and Bootstrap Material Design.</p>
+            </figcaption>  
+          </figure>
+        </a>
       </div>
     </section>
   );
