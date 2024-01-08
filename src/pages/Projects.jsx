d9e7355a74fb4e246d9c766473cce001cf339313
Nhi Evie Hoang <142361267+eviehoang@users.noreply.github.com>
2024-01-08 09:45:54 -0800

    Update project links in Projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -3,14 +3,17 @@ import operaLanding from "../assets/project-images/opera-landing-page.png"
 
 
 function Projects() {
+const recipeRoloLink = "https://recipe-rolodex-d7c0cb19d5d1.herokuapp.com/";
+const operaLandingLink = "https://operabook.netlify.app/";
+
   return (
     <section className="flex items-center flex-wrap justify-center m-8 px-8 z-40">
       <div className="m-auto container mb-0">
         <h1 id="projectHeading">Projects</h1>
       </div>
 
-      <div className="flex flex-wrap flex-reverse justify-center mt-0 gap-9 py-12 w-5/6">
-        <a href="https://recipe-rolodex-d7c0cb19d5d1.herokuapp.com/" className="projectCard">
+      <div className="flex flex-wrap-reverse justify-center mt-0 gap-9 py-12 w-5/6">
+        <a href={recipeRoloLink} className="projectCard">
           <figure>
             <img src={recipeRolo} alt="Recipe Rolodex Site" />
             <figcaption>Recipe Rolodex
@@ -60,7 +63,7 @@ function Projects() {
           </figure>
         </a>
 
-        <a href="https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png" className="projectCard">
+        <a href={operaLandingLink} className="projectCard">
           <figure>
             <img src={operaLanding} alt="Opera Landing Page" />
             <figcaption>Landing Page: Pre-launch
